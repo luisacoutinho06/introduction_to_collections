8a9068a4c19c103e3a4c0df44e730c4b68d1a88a
Luisa Silva Coutinho <luisa.coutinho@minasmaquinas.com.br>
2024-03-18 16:41:05 -0300

    Inserindo projetos 1.1

---
+++ C#_COLLECTIONS PARTE 1/6 - SetInModelLearning/SetInModelLearning.cs
@@ -0,0 +1,94 @@
+﻿namespace _6___SetInModelLearning
+{
+    class SetInModelLearning
+    {
+        static void Main(string[] args)
+        {
+            // Declaração do curso:
+            Curso csharpColecoes = new Curso("C# Colecoes", "Marcelo Oliveira");
+
+            //... e adcionando as 3 aulas a esse curso
+            // Trabalhando com listas - 21 minutos;
+            // Criando uma aula - 20 minutos;
+            // Modelando com coleções - 24 minutos
+
+            csharpColecoes.Adiciona(new Aula("Trabalhando com listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com coleções", 24));
+
+            // Instanciando os 3 alunos com suas matrículas:
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rfael Nercessian", 17645);
+
+            // Matriculando os alunos no curso, criando um método:
+            csharpColecoes.Matricula(a1);
+            csharpColecoes.Matricula(a2);
+            csharpColecoes.Matricula(a3);
+
+            // Imprimindo os alunos matriculados
+            Console.WriteLine("Imprimindo os alunos matriculados:");
+            foreach (var aluno in csharpColecoes.Alunos)
+            {
+                Console.WriteLine(aluno.ToString());
+            }
+
+            // Imprimir: "O aluno a1 está matriculado?"
+            Console.WriteLine($"O aluno a1 {a1.Nome} está matriculado?");
+
+            // Criar um método EstaMatriculado:
+            Console.WriteLine(csharpColecoes.EstaMatriculado(a1));
+
+            // Instanciar uma aluna (Vanessa Tonini):
+            Aluno tonini = new Aluno("Vanessa Tonini", 34672);
+
+            // Não é reconhecido que essa segunda instância é igual a primeira instância 
+            Console.WriteLine("Tonini está matriculada? " + csharpColecoes.EstaMatriculado(tonini));
+
+            // Verificação se a1 == a Tonini?
+            Console.WriteLine("a1 == a Tonini?");
+            Console.WriteLine(a1 == tonini);
+
+            // Verificação utilizando o método equals:
+            Console.WriteLine("a1 é equals a Tonini?");
+            Console.WriteLine(a1.Equals(tonini));
+
+            // Limpando o console:
+            Console.Clear();
+
+            // Já possuímos um método para saber se o aluno está matriculado.
+            // Agora é necessário buscar o aluno pelo número de matrícula.
+            // Deve-se responder esta pergunta "Quem é o aluno com a matrícula 5617?":
+            Console.WriteLine("Quem é o aluno com a matrícula 5617?");
+
+            // Implementando um novo método para buscar o aluno pela matrícula, "Curso.BuscaMatriculado":
+            Aluno aluno5617 = csharpColecoes.BuscaMatriculado(5617);
+
+            // Imprimindo o aluno 5617 que foi encontrado:
+            Console.WriteLine("aluno5617: " + aluno5617);
+
+            // Implementando um método de busca mais eficiente.
+            // Implementando uma nova coleção: dicionário. Um dicionário permite associar uma chave (no caso, matrícula) a um valor (o aluno).
+
+
+            // O que acontece quando se puxa um aluno inexistente? "Quem é o aluno 5618"?
+            Console.WriteLine("Quem é o aluno 5618");
+            Console.WriteLine(csharpColecoes.BuscaMatriculado(5618));
+
+            // E se tentarmos adicionar outro aluno com a mesma chave 5617? 
+            Aluno fabio = new Aluno("Fabio Gushiken", 5617);
+            //csharpColecoes.Matricula(fabio); 
+            // ------> Ocorre um erro, pois uma das características do dicionário é o fato de que a chave é única.
+
+            // E se quisermos trocar o aluno que possui a chave específica?
+            csharpColecoes.SubstituiAluno(fabio);
+
+            // Pergunta: "Quem é o aluno 5617 agora?"
+            Console.WriteLine("Quem é o aluno 5617 agora?");
+            Console.WriteLine(csharpColecoes.BuscaMatriculado(5617));
+
+
+
+        }
+    }
+}
---
+++ C#_COLLECTIONS PARTE 1/ListReadOnlyLearning/Curso.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListReadOnlyLearning
+{
+    // Declaração de propriedades, por meio de uma classe com arquivo separado:
+    class Curso
+    {
+        // Primeira propriedade que foi protegida de códigos externos, utilizando o ReadOnlyCollection:
+        private IList<Aula> aulas;
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+
+        // Construtor para iniciar a lista de aulas:
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            this.aulas = new List<Aula>();
+
+        }
+
+        // Segunda propriedade da classe curso:
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+
+        // Terceira propriedade da classe curso:
+        private string instrutor;
+        public string Instrutor
+        {
+            get { return instrutor; }
+            set { instrutor = value; }
+        }
+
+        // Propriedade que calcula o tempo total do curso inteiro:
+        public int TempoTotal
+        {
+            // Primeira forma de realizar o cálculo:
+
+            get
+            {
+                //    int total = 0;
+                //    foreach (var aula in aulas)
+                //    {
+                //        total += aula.Tempo;
+                //    }
+                //    return total;
+
+                // LINQ = Language Integrated Query.
+                // Consulta integrada á linguagem.
+                // Segunda forma de realizar o cálculo:
+                return aulas.Sum(aula => aula.Tempo);
+
+            }
+        }
+
+        // Método criado para adcionar um curso após a proteção do ReadOnlyCollection:
+        internal void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+        // Chamaremos o override pois ele é uma operação de sobrescrever um método que já existe em uma classe (no caso a classe obj):
+        public override string ToString()
+        {
+            return $"Curso:{nome} , Tempo:{TempoTotal} , Aulas: {string.Join(",", aulas)}.";
+        }
+    }
+}
